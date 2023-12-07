fab4e7264084e67c92cc3049e52a420d8b6946e1
eisaku <yeisaku@gmail.com>
2023-12-07 14:54:06 +0900

    Update streamlit_app.py | JP->en

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 独自のフルーツスムージーを作ろう 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
