8c8d8127625d5aa063ddf1449873245c741e77d7
eisaku <yeisaku@gmail.com>
2023-12-07 14:49:10 +0900

    Update streamlit_app.py | add Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Prents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omege 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
