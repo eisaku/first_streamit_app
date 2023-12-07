7e3a55516b4d77058585154e4ca7e60d0bb9d779
eisaku <yeisaku@gmail.com>
2023-12-07 15:02:14 +0900

    Update streamlit_app.py | add multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,13 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# ここに選択リストを置き、含めたい果物を選択できるようにしましょう。 
+streamlit.multiselect("Pick some fruits:", list( my_fruit_list.index))
+
+# ページに表を表示します。
 streamlit.dataframe(my_fruit_list)
